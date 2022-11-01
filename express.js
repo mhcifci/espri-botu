8f89d8d6ed4b87313a29e97991d61d56b78532be
mhcifci <mhcifci@gmail.com>
2022-11-01 16:54:35 +0300

    test

---
+++ express.js
@@ -0,0 +1,33 @@
+const TOKEN = process.env.TELEGRAM_TOKEN || '5408801300:AAGDGGTh92QQiUmKCx5qx2f8qvGrrwidGtQ';
+const url = 'https://esprituel-ayi.herokuapp.com/';
+const port = process.env.PORT || 3000;
+
+const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+
+// No need to pass any parameters as we will handle the updates with Express
+const bot = new TelegramBot(TOKEN);
+
+// This informs the Telegram servers of the new webhook.
+bot.setWebHook(`${url}/bot${TOKEN}`);
+
+const app = express();
+
+// parse the updates to JSON
+app.use(express.json());
+
+// We are receiving updates at the route below!
+app.post(`/bot${TOKEN}`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
+
+// Start Express Server
+app.listen(port, () => {
+  console.log(`Express server is listening on ${port}`);
+});
+
+// Just to ping!
+bot.on('message', msg => {
+  bot.sendMessage(msg.chat.id, 'I am alive!');
+});
